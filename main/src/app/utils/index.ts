cfcde3fa246c11dd038e4f92ca61b1f32a017380
Ryan Snorek <ryan.snorek@manamonitoring.com>
2023-05-28 11:25:00 -0400

    utils + typeguards

--- main/src/app/utils/index.ts
+++ main/src/app/utils/index.ts
@@ -1,3 +1,6 @@
-export const isWordMatch = (word: any, input: string) => {
+export const validateWordMatch = (word: any, input: string) => {
   return word.synonyms.includes(input);
-}
+};
+export const isEmpty = (obj: object) => {
+  return Object.keys(obj).length === 0;
+};
